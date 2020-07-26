0328ba141bd2186c394fb629521884cae00615fd
Alexander Ignition <izh.sever@gmail.com>
2020-07-26 14:54:09 +0300

    Refactored HTTP routes

--- Sources/Compote/Routing/RequestRoute.swift
+++ Sources/Compote/Routing/RequestRoute.swift
@@ -1,27 +1,52 @@
 import NIO
 import NIOHTTP1
 
-public typealias RequestRouteHanlder = (RouteContext) throws -> EventLoopFuture<HTTPResponse>
-
-public func notImplemented(_ context: RouteContext) -> EventLoopFuture<HTTPResponse> {
-    context.response(status: .notImplemented)
+@inlinable
+public func GET(_ path: PathPattern = .init(), handler: RouteHanlder? = nil) -> RequestRoute {
+    RequestRoute(.GET, path, handler: handler)
 }
+
+@inlinable
+public func POST(_ path: PathPattern = .init(), handler: RouteHanlder? = nil) -> RequestRoute {
+    RequestRoute(.POST, path, handler: handler)
+}
+
+@inlinable
+public func PUT(_ path: PathPattern = .init(), handler: RouteHanlder? = nil) -> RequestRoute {
+    RequestRoute(.PUT, path, handler: handler)
+}
+
+@inlinable
+public func PATCH(_ path: PathPattern = .init(), handler: RouteHanlder? = nil) -> RequestRoute {
+    RequestRoute(.PATCH, path, handler: handler)
+}
+
+@inlinable
+public func DELETE(_ path: PathPattern = .init(), handler: RouteHanlder? = nil) -> RequestRoute {
+    RequestRoute(.DELETE, path, handler: handler)
+}
+
+// MARK: - Basic
 
 public struct RequestRoute: Route {
     public var route: Never { fatalError() }
 
     public let method: NIOHTTP1.HTTPMethod
     public var path: PathPattern
-    public let handler: RequestRouteHanlder
+    public let handler: RouteHanlder
 
     public init(
         _ method: NIOHTTP1.HTTPMethod,
         _ path: PathPattern = .init(),
-        handler: @escaping RequestRouteHanlder = notImplemented
+        handler: RouteHanlder?
     ) {
         self.method = method
         self.path = path
-        self.handler = handler
+        self.handler = handler ?? Self.notImplemented
+    }
+
+    private static let notImplemented: RouteHanlder = {
+        $0.response(status: .notImplemented)
     }
 
     func match(_ context: inout RouteContext) -> Bool {
@@ -44,89 +69,3 @@ extension RequestRoute: CustomStringConvertible {
     }
 }
 
-// MARK: - HTTP method
-
-public struct GET: Route {
-    private let path: PathPattern
-    private let handler: RequestRouteHanlder
-
-    public init(
-        _ path: PathPattern = .init(),
-        handler: @escaping RequestRouteHanlder = notImplemented
-    ) {
-        self.path = path
-        self.handler = handler
-    }
-
-    public var route: some Route {
-        RequestRoute(.GET, path, handler: handler)
-    }
-}
-
-public struct POST: Route {
-    private let path: PathPattern
-    private let handler: RequestRouteHanlder
-
-    public init(
-        _ path: PathPattern = .init(),
-        handler: @escaping RequestRouteHanlder = notImplemented
-    ) {
-        self.path = path
-        self.handler = handler
-    }
-
-    public var route: some Route {
-        RequestRoute(.POST, path, handler: handler)
-    }
-}
-
-public struct PUT: Route {
-    private let path: PathPattern
-    private let handler: RequestRouteHanlder
-
-    public init(
-        _ path: PathPattern = .init(),
-        handler: @escaping RequestRouteHanlder = notImplemented
-    ) {
-        self.path = path
-        self.handler = handler
-    }
-
-    public var route: some Route {
-        RequestRoute(.PUT, path, handler: handler)
-    }
-}
-
-public struct PATCH: Route {
-    private let path: PathPattern
-    private let handler: RequestRouteHanlder
-
-    public init(
-        _ path: PathPattern = .init(),
-        handler: @escaping RequestRouteHanlder = notImplemented
-    ) {
-        self.path = path
-        self.handler = handler
-    }
-
-    public var route: some Route {
-        RequestRoute(.PATCH, path, handler: handler)
-    }
-}
-
-public struct DELETE: Route {
-    private let path: PathPattern
-    private let handler: RequestRouteHanlder
-
-    public init(
-        _ path: PathPattern = .init(),
-        handler: @escaping RequestRouteHanlder = notImplemented
-    ) {
-        self.path = path
-        self.handler = handler
-    }
-
-    public var route: some Route {
-        RequestRoute(.DELETE, path, handler: handler)
-    }
-}
